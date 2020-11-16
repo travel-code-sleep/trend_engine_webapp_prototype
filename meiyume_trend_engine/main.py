fbc4d416e0fe15276322791e7e913087b022ac73
Junhui Xu <bizjun@nus.edu.sg>
2020-11-16 09:20:43 +0800

    revert the debugging edit

--- meiyume_trend_engine/main.py
+++ meiyume_trend_engine/main.py
@@ -4609,5 +4609,5 @@ def render_page_content(pathname):
 
 
 if __name__ == "__main__":
-    # app.run_server()
-    app.run_server(debug=True, host="127.0.0.5")
+    app.run_server()
+    # app.run_server(debug=True, host="127.0.0.5")
